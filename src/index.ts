133f4bd8185ad8d0f0f4b3d8c49e4b7e8213a278
VictorGugg <victor.guggenbuhl@gmail.com>
2023-04-12 09:46:36 +0200

    Practicing with simple GraphQL

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,44 @@
-import express from "express";
-import cors from "cors";
 import dataSource from "./utils";
 
 // import { Wilder } from "./entity/Wilder";
 // import { Skill } from "./entity/Skill";
 
-import wilderController from "./controller/wilderController";
-import skillController from "./controller/skillController";
-import gradeController from "./controller/gradeController";
+import { Wilder } from "./entity/Wilder";
 
-const PORT = 3030;
+import { ApolloServer, gql } from "apollo-server";
 
-const app = express();
+const typeDefs = gql`
+  type Wilder {
+    name: String
+    city: String
+  }
 
-// using cors to allow connection from other IP (front-end = port 3000, API = port 3030)
-app.use(cors());
-// allows the app to read .json
-app.use(express.json());
+  type Query {
+    wilder: [Wilder]
+  }
+`;
 
-// Arrow function "(arguments) => {}" is equivalent to "function (arguments) {}"
-// req = request, res = response
-app.get("/", (req, res) => {
-  res.send("Hello World, this is the end !");
+const wilders = async (): Promise<Wilder[]> => {
+  return await dataSource.getRepository(Wilder).find();
+};
+
+const resolvers = {
+  Query: {
+    getWilders: async () => await wilders(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
-// Wilder Routes
-app.post("/api/wilder", wilderController.create);
-app.get("/api/wilder", wilderController.read);
-app.put("/api/wilder/:id", wilderController.update);
-app.delete("/api/wilder/:id", wilderController.delete);
-
-// app.put("/api/wilder-add-skills", wilderController.addSkills);
-
-app.put("/api/wilder-rate-skill", wilderController.rateSkill);
-
-// Skill Routes
-app.post("/api/skill", skillController.create);
-app.get("/api/skill", skillController.read);
-app.put("/api/skill/:id", skillController.update);
-app.delete("/api/skill/:id", skillController.delete);
-
-// Grade Routes
-app.post("/api/grade", gradeController.create);
-app.get("/api/grade", gradeController.read);
-app.put("/api/grade/:id", gradeController.update);
-app.delete("/api/grade/:id", gradeController.delete);
-
 const start = async (): Promise<void> => {
-  // await makes the script wait for the action to execute before continuing running
   await dataSource.initialize();
-
-  // creates new entry in SQL db
-  // dataSource.getRepository(Wilder).save({ name: 'First Wilder'});
-
-  // Starting Server (here on port 3030)
-  app.listen(PORT, () => console.log("Server Started on port ", PORT));
+  const { url } = await server.listen();
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  console.log(`Server started on ${url}`);
 };
 
 void start();
+// The `listen` method launches a web server.
